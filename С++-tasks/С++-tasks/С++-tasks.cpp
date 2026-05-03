f6062018a08c2216a190add8b5d3c44f5fee9c17
Gan2Kirich <kiriltchekulae@yandex.ru>
2026-05-03 22:30:35 +0300

    Add files via upload
    Задачи с 11-16

---
+++ С++-tasks/С++-tasks/С++-tasks.cpp
@@ -0,0 +1,49 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+int main()
+{
+    // Задача 1.1
+    /*
+    int aga = 31;
+    int ag = 18;
+    int a = 79;
+    std::cout << aga << " " << ag << " " << a;
+    */
+    // Задача 1.2
+    /*
+    int aga = 47;
+    int ag = 52;
+    int a = 150;
+    std::cout << aga << "  " << ag << "  " << a;
+    */
+    // Задача 1.3
+    /*
+    int aga = 50;
+    int ag = 10;
+    std::cout << aga << "\n";
+    std::cout << ag ;
+    */
+    // Задача 1.4
+    /*
+    int aga = 5;
+    int ag = 10;
+    int a = 21;
+    std::cout << aga << "\n" << ag << "\n" << a;
+    */
+    // Задача 1.5
+    /*
+    int aga = 1;
+    int ag = 2;
+    std::cout << aga << "\n" << "\n" << ag;
+    */
+    // Задача 1.6
+    /*
+    double pi = M_PI;
+    std::cout << std::fixed << std::setprecision(3);
+    std::cout << M_PI;
+    */
+
+}
