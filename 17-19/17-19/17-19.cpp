9f7a8f7bae336de2e65d039b226f7316678ae156
Gan2Kirich <kiriltchekulae@yandex.ru>
2026-05-04 22:38:02 +0300

    Add files via upload
    Задачи с 1.7 по 1.9

---
+++ 17-19/17-19/17-19.cpp
@@ -0,0 +1,32 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <clocale>
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    // Задача 1.7
+    /*
+    double ee = M_E;
+    std::cout << std::fixed << std::setprecision(1);
+    std::cout << M_E;
+    */
+    // Задача 1.8
+    /*
+    int a;
+    std::cout << "Введите число: ";
+    std::cin >> a;
+    std::cout  << "Вы ввели число " << a;
+    */
+    // Задача 1.9
+    /*
+    int a;
+    std::cout << "Введите число: ";
+    std::cin >> a;
+    std::cout << a << " - Вот такое число вы ввели" ;
+    */
+}
+
