085d7e14631c6a07ede49cf81cd7c2451ed9713e
Gan2Kirich <kiriltchekulae@yandex.ru>
2026-05-05 22:29:52 +0300

    Add files via upload
    Задачи с 1.11 по 1.13

---
+++ 111-113/111-113/111-113.cpp
@@ -0,0 +1,34 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <clocale>
+#include <string>
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    // Задача 1.11
+    /*
+    std::string name;
+    std::cout << "Введите название футбольной команды: ";
+    std::getline(std::cin, name);
+    std::cout << name << "- это чемпион!";
+    */
+    // Задача 1.12
+    /*
+    std::string name;
+    std::cout << "Введите имя: ";
+    std::getline(std::cin, name);
+    std::cout << "Привет, " << name << "!";
+    */
+    // Задача 1.13
+    /*
+    int a;
+    std::cout << "Введите число: ";
+    std::cin >> a;
+    std::cout << "Следующее за числом " << a<< " число " << a + 1 << "\n";
+    std::cout << "Для числа " << a << "предыдущее число " << a - 1 << "\n";
+    */
+}
